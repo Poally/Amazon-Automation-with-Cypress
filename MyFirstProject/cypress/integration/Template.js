3bc7195312eee3b9b08ae1f0eca335891bce4206
Elsoak <80517191+Elsoak@users.noreply.github.com>
2021-07-17 23:57:26 +0700

    Minor Push
    Add Assertion and edit plugin files

--- MyFirstProject/cypress/integration/Template.js
+++ MyFirstProject/cypress/integration/Template.js
@@ -1,8 +1,23 @@
 /// <reference types="cypress" />
-
-it("first test with google", function() {
-    cy.visit('http://google.com')
-    cy.get('.gLFyf').type('Elsa{enter}') 
-    //{} for command action tab up down or enter
+ 
+const cypress = require("cypress")
+ 
+describe('Amazon Automation TestSuite', function(){
+it("Get Lowest Price Goods on Amazon TestCase", function() {
+    cy.visit(Cypress.env('prepare[url]'))
+    // cy.visit('http://amazon.com')
+    // cy.get('.gLFyf',{timeout:6000}).type('Elsa{enter}') 
+    cy.get('#nav-logo-sprites').should('be.visible')
+    cy.get('#glow-ingress-line2')
+        .contains('Thailand',{matchCase: false})
+        .should('have.class','nav-line-2 nav-progressive-content')
+        .should('be.visible')
+    cy.get('#nav-hamburger-menu').invoke('attr','role').should('equal','button')
+ 
+ 
+ 
     
+ 
+    })
 })
+
