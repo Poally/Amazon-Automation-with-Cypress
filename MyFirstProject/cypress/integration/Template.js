6a1ff9d4b048fefcdd72def1b8423e8a5104b35f
Elsoak <80517191+Elsoak@users.noreply.github.com>
2021-07-18 23:57:30 +0700

    Add Fixture
    Add Variable data in json from Fixture

--- MyFirstProject/cypress/integration/Template.js
+++ MyFirstProject/cypress/integration/Template.js
@@ -1,19 +1,20 @@
 /// <reference types="cypress" />
  
-const cypress = require("cypress")
- 
 describe('Amazon Automation TestSuite', function(){
 it("Get Lowest Price Goods on Amazon TestCase", function() {
-    cy.visit(Cypress.env('prepare[url]'))
-    // cy.visit('http://amazon.com')
+    cy.visit('http://amazon.com')
     // cy.get('.gLFyf',{timeout:6000}).type('Elsa{enter}') 
     cy.get('#nav-logo-sprites').should('be.visible')
     cy.get('#glow-ingress-line2')
         .contains('Thailand',{matchCase: false})
         .should('have.class','nav-line-2 nav-progressive-content')
         .should('be.visible')
-    cy.get('#nav-hamburger-menu').invoke('attr','role').should('equal','button')
- 
+    cy.get('#nav-hamburger-menu').invoke('attr','role').should('equal','button')    
+    cy.fixture('Goods1.json').then((Goods1) =>{
+        //pull data from Goods1.json pass as Goods1 obj
+        cy.get('#twotabsearchtextbox').type(Goods1.name).type('{enter}')
+    })
+    
  
  
     
